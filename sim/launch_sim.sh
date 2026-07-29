f4ea9cef71dfec34b65d9dc4ab5386c737bc948e
joelofrese <joelofrese01@gmail.com>
2026-07-28 22:01:06 -0700

    Remove legacy simulation launchers

--- sim/launch_sim.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Launch PX4 SITL + Gazebo.
-cd ~/Code/Croppie/PX4-Autopilot
-source .venv/bin/activate
-export DYLD_LIBRARY_PATH=/opt/homebrew/lib:$DYLD_LIBRARY_PATH
-make px4_sitl gz_x500
